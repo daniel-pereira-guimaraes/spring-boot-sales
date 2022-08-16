dfced5a8e4e555b7a951a5d2059fd10a4bee3fba
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-08-16 08:37:41 -0400

    Removed unnecessary imports.

--- src/main/java/com/example/spring/boot/sales/dto/SaleSummaryByDateDTO.java
+++ src/main/java/com/example/spring/boot/sales/dto/SaleSummaryByDateDTO.java
@@ -3,10 +3,6 @@ package com.example.spring.boot.sales.dto;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 public interface SaleSummaryByDateDTO {
 	
 	LocalDate getDate();

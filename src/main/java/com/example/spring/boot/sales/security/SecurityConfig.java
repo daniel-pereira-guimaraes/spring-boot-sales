73274221ba394873bd8783e44d1515e153f5ac25
Daniel Pereira Guimarães <danielpgbrasil@gmail.com>
2022-08-16 14:01:01 -0400

    Swaped WebSecurityConfigurerAdapter for SecurityFilterChain.

--- src/main/java/com/example/spring/boot/sales/security/SecurityConfig.java
+++ src/main/java/com/example/spring/boot/sales/security/SecurityConfig.java
@@ -1,23 +1,26 @@
 package com.example.spring.boot.sales.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.csrf().disable()
-			.authorizeRequests()
-			.antMatchers("/login").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.httpBasic();
-	}
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .build();
+    }
 	
 	public static BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
